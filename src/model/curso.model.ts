2fdc91e9f34ad6e7580da3649e553615ccb0add9
SaquiNda <eliasdavidsch@hotmail.com>
2023-05-30 20:37:28 -0500

    nuevo

---
+++ src/model/curso.model.ts
@@ -0,0 +1,38 @@
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('curso', { schema: 'curso' })
+export class CursoEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @CreateDateColumn({
+    name: 'create_at', //trabajamos con guion bajo
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createAt: Date;
+  @UpdateDateColumn({
+    name: 'update_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updateAt: Date;
+  @DeleteDateColumn({
+    name: 'delete_at',
+    type: 'timestamp',
+    nullable: true,
+  })
+  deleteAt: Date;
+  @Column('varchar',{
+    name:'paralelo',
+    nullable:false,
+    comment:'nombre del paralelo'
+  })
+  paralelo:string;
+  @Column('numeric',{
+    name:'numeroParticipantes',
+    nullable:false,
+    comment:'numero de participantes'
+  })
+  numeroParticipantes:number;
+}
