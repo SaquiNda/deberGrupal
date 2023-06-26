8b1b4b03abac09e89f801979b0f14dca6205fff1
Daniel <danielvera614@gmail.com>
2023-06-26 15:56:47 -0500

    agrega shared

---
+++ src/Curso/shared/enums/catalogue.enum.ts
@@ -0,0 +1,8 @@
+export enum CatalogueTypeEnum {
+    CATALOGUE = 'CATALOGUE',
+        PRODUCT = 'PRODUCT'
+  }
+  
+  export enum CatalogueStateEnum {
+    ACTIVE = 'ACTIVE',
+  }
---
+++ src/Curso/shared/enums/index.ts
@@ -0,0 +1,4 @@
+export * from './catalogue.enum'
+export * from './config.enum'
+export * from './data-source.enum'
+export * from './repository.enum'
